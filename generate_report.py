304e4f4fc4a777a7dd1fc3aa4e9bf664de3da81b
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2013-10-29 13:15:43 +0100

    Removed unused functions

--- generate_report.py
+++ generate_report.py
@@ -127,43 +127,6 @@ class Config:
 		except:
 			self.report_graph_width = '1200'
 
-#def get_options():
-#	""" command-line options """
-
-#	usage = "usage: %prog [options]"
-#	OptionParser = optparse.OptionParser
-#	parser = OptionParser(usage)
-
-#	parser.add_option("-s", "--server", action="store", type="string", dest="server", help="Zabbix Server URL (REQUIRED)")
-#	parser.add_option("-u", "--username", action="store", type="string", dest="username", help="Username (Will prompt if not given)")
-#	parser.add_option("-p", "--password", action="store", type="string", dest="password", help="Password (Will prompt if not given)")
-
-#	options, args = parser.parse_args()
-
-#	if not options.server:
-#		show_help(parser)
-
-#	if not options.username:
-#		options.username = raw_input('Username: ')
-
-#	if not options.password:
-#		options.password = getpass()
-
-	# apply clue to user...
-#	if not options.username and not options.password:
-#		show_help(parser)
-
-#	return options, args
-
-#def show_help(p):
-#	p.print_help()
-#	print "NOTE: Zabbix 1.8.0 doesn't check LDAP when authenticating."
-#	sys.exit(-1)
-
-#def errmsg(msg):
-#	sys.stderr.write(msg + "\n")
-#	sys.exit(-1)
-
 def selectHostgroup():
 	teller = 0
 	hostgroups = {}
